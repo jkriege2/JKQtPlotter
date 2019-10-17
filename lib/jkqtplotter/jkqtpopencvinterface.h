92c1851c8ce611f2a6a540bb15277deefb2bcfb0
jkriege2 <jan@jkrieger.de>
2019-10-17 23:49:50 +0200

    bugfix: OpenCV-interface used hidden function

--- lib/jkqtplotter/jkqtpopencvinterface.h
+++ lib/jkqtplotter/jkqtpopencvinterface.h
@@ -77,8 +77,7 @@ inline size_t JKQTPCopyCvMatToColumn(JKQTPDatastore* datastore, const cv::Mat& m
     else if (CV_MAT_DEPTH(mat.type())==CV_8U) JKQTPDatastore_Helper::copyDataFromMat<uint8_t>(d, mat, channel);
     else throw std::runtime_error("datatype of cv::Mat not supported by JKQTPDatastore::copyImageToColumn()");
 
-    size_t itemid=datastore->addInternalItem(d, N);
-    return datastore->addColumnForItem(itemid, 0, name);
+    return datastore->addInternalColumn(d, N, name);
 }
 
 
