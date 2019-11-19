66ab42e05553f6a756597492aac8218c6420e124
Ondřej Čertík <ondrej@certik.us>
2019-11-18 17:24:03 -0700

    Fix the path

--- ci/test_travis.sh
+++ ci/test_travis.sh
@@ -11,7 +11,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
 cmake --build . --target install
 
 if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
-    cd test_project
+    cd ../ci/test_project
     cmake -DCMAKE_PREFIX_PATH=$CONDA_PREFIX .
     cmake --build .
     ldd test_project | grep "not found"
