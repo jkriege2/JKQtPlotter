e0fbc04c6a16efebe4a1ed927a531e62486a6c0b
Ondřej Čertík <ondrej@certik.us>
2019-11-18 17:16:19 -0700

    Add a test app

---
+++ ci/test_project/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+
+#include "jkqtplotter/graphs/jkqtpscatter.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    JKQTPXYLineGraph* graph;
+
+    return app.exec();
+}
