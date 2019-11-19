12851896edf84e05cf7da054e79a0baa0e848800
Ondřej Čertík <ondrej@certik.us>
2019-11-18 17:20:43 -0700

    Test on Travis

--- ci/test_travis.sh
+++ ci/test_travis.sh
@@ -9,3 +9,10 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
 cmake --build . --target install
+
+if [[ "${TRAVIS_OS_NAME}" != "osx" ]]; then
+    cd test_project
+    cmake -DCMAKE_PREFIX_PATH=$CONDA_PREFIX .
+    cmake --build .
+    ldd test_project | grep "not found"
+fi
