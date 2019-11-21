6d8139e9960c83fd7ea3bf22b247a7c315e8bcd0
jkriege2 <jan@jkrieger.de>
2019-11-21 08:01:17 +0100

    add missing compiler flag

--- cmake/jkqtplotter_common_compilersettings.cmake
+++ cmake/jkqtplotter_common_compilersettings.cmake
@@ -10,8 +10,10 @@ if(MINGW)
 elseif(MSVC)
   # COMPILER-SETTINGS FOR MS VISUAL C++
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
-    # To enable M_PI, M_E,...
+  # To enable M_PI, M_E,...
   add_definitions(/D_USE_MATH_DEFINES)
+  # To Prevent Errors with min() and max()
+  add_definitions(/DNOMINMAX)
   # To fix error: C2338: va_start argument must not
   # have reference type and must not be parenthesized
   add_definitions(/D_CRT_NO_VA_START_VALIDATION)
