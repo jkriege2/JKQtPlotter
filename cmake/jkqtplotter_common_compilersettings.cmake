52d334919995ea534c8f26dd199ff1c131f85cc3
jkriege2 <jan@jkrieger.de>
2020-06-28 15:53:26 +0200

    improvements in CMake setup: - removed more global settings (these are now target-specific) - exporting now Config.cmake, which contains find_package(Qt) and loads a separate Targets.cmake

--- cmake/jkqtplotter_common_compilersettings.cmake
+++ cmake/jkqtplotter_common_compilersettings.cmake
@@ -4,18 +4,6 @@ cmake_minimum_required(VERSION 3.1)
 set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_CXX_STANDARD_REQUIRED on)
 
-if(MINGW)
-  # COMPILER-SETTINGS FOR MINGW
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fexceptions")
-elseif(MSVC)
-  # COMPILER-SETTINGS FOR MS VISUAL C++
-  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
-  # To fix error: C2338: va_start argument must not
-  # have reference type and must not be parenthesized
-  add_definitions(/D_CRT_NO_VA_START_VALIDATION)
-  add_definitions(/DNOMINMAX)
-endif()
-
 if(NOT APPLE)
     set(CMAKE_INSTALL_RPATH $ORIGIN)
 endif()
