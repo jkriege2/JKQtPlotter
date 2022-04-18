8835febb966ffa7804feeddbddf1c340db83010c
jkriege2 <jan@jkrieger.de>
2022-04-18 23:10:05 +0200

    modified CMakeLists.txt to match Qt6

--- cmake/jkqtplotter_common_qtsettings.cmake
+++ cmake/jkqtplotter_common_qtsettings.cmake
@@ -4,4 +4,5 @@ set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
 set(CMAKE_AUTOUIC ON)
 
-find_package(Qt5 5.0 REQUIRED Core Gui Widgets PrintSupport Svg Xml OpenGL)
+find_package(QT NAMES Qt6 Qt5 COMPONENTS Core Gui Widgets PrintSupport Svg Xml OpenGL REQUIRED)
+find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Core Gui Widgets PrintSupport Svg Xml OpenGL REQUIRED)
--- cmake/jkqtplotter_deployqt.cmake
+++ cmake/jkqtplotter_deployqt.cmake
@@ -1,7 +1,7 @@
 function(jkqtplotter_deployqt TARGET_NAME)
     #use windeploy
     if (WIN32)
-        get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+        get_target_property(_qmake_executable Qt${QT_VERSION_MAJOR}::qmake IMPORTED_LOCATION)
         get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
         find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
         find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
