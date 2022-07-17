fed64d0a513befbb5d81eb5df4e293ad935d5fce
sufuk <sufukguler@gmail.com>
2022-07-17 14:10:14 +0300

    added QT_NO_PRINTER def

--- cmake/jkqtplotter_common_compilersettings.cmake
+++ cmake/jkqtplotter_common_compilersettings.cmake
@@ -4,3 +4,7 @@ cmake_minimum_required(VERSION 3.1)
 if(NOT APPLE)
     set(CMAKE_INSTALL_RPATH $ORIGIN)
 endif()
+
+if (NOT ${QT_PRINT_SUPPORT})
+    add_compile_definitions(QT_NO_PRINTER)
+endif ()
