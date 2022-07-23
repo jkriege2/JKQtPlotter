cf628b38abaf5a8b6b4176b8c8d1d6acaaa1c5fc
jkriege2 <jan@jkrieger.de>
2022-07-23 21:24:53 +0200

    bugfixed OpenCV install for newer MSVC OpenCV-builds

--- cmake/jkqtplotter_deployopencv.cmake
+++ cmake/jkqtplotter_deployopencv.cmake
@@ -1,12 +1,20 @@
 function(jkqtplotter_deployopencv TARGET_NAME)
-    #use windeploy
     if(OpenCV_FOUND)
         if(WIN32)
-            add_custom_command(TARGET ${EXENAME} POST_BUILD        # Adds a post-build event the TARGET
-                    COMMAND ${CMAKE_COMMAND} -E copy_directory
-                    "${_OpenCV_LIB_PATH}"
-                    $<TARGET_FILE_DIR:${EXENAME}>
+            if (EXISTS ${_OpenCV_LIB_PATH})
+              add_custom_command(TARGET ${EXENAME} POST_BUILD        # Adds a post-build event the TARGET
+                      COMMAND ${CMAKE_COMMAND} -E copy_directory
+                      "${_OpenCV_LIB_PATH}"
+                      "$<TARGET_FILE_DIR:${EXENAME}>"
                     )
+            endif()
+            if (EXISTS ${OpenCV_INSTALL_PATH}/bin)
+              add_custom_command(TARGET ${EXENAME} POST_BUILD        # Adds a post-build event the TARGET
+                      COMMAND ${CMAKE_COMMAND} -E copy_directory
+                      "${OpenCV_INSTALL_PATH}/bin"
+                      "$<TARGET_FILE_DIR:${EXENAME}>"
+                    )
+            endif()
         endif(WIN32)
      endif(OpenCV_FOUND)
 endfunction(jkqtplotter_deployopencv)
