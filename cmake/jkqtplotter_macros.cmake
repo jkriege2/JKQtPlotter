ddcc9ea4628321a051a1d28618044f58c40eae74
jkriege2 <jan@jkrieger.de>
2022-10-06 11:54:18 +0200

    BUGFIX: Error for PDBs when building STATIC libs on MSVC BUGFIX: a variable in the ...Config.cmake-files was not set correctly

--- cmake/jkqtplotter_macros.cmake
+++ cmake/jkqtplotter_macros.cmake
@@ -1,13 +1,15 @@
-function(jkqtplotter_installlibrary lib_name libIncludeSubdir)
+function(jkqtplotter_installlibrary lib_name libIncludeSubdir BuildTypePart)
     install(TARGETS ${lib_name} EXPORT ${lib_name}_TARGETS
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${libIncludeSubdir}
     )
-    if(WIN32 AND MSVC AND (NOT MSVC_VERSION LESS 1600) AND (NOT CMAKE_VERSION VERSION_LESS "3.1"))
+    if(WIN32 AND MSVC AND (NOT MSVC_VERSION LESS 1600) AND (NOT CMAKE_VERSION VERSION_LESS "3.1") AND (NOT BuildTypePart STREQUAL ""))
+        message(STATUS "?????????????? INSTALL PDF BuildTypePart=${BuildTypePart}")
         install(FILES $<TARGET_PDB_FILE:${lib_name}> DESTINATION ${CMAKE_INSTALL_BINDIR} OPTIONAL)
     endif()
+    set(JKQTP_CURRENT_TARGET_SHAREDPART "${BuildTypePart}")
     set(JKQTP_CURRENT_TARGET_FILENAME "${lib_name}Targets.cmake")
     configure_file(LibTarget.cmake.in "${CMAKE_CURRENT_BINARY_DIR}/${lib_name}Config.cmake" @ONLY)
     install(EXPORT ${lib_name}_TARGETS
