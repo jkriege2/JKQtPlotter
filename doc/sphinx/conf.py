da394e4c6f9bfdcf717628e0ff6d75982250f153
jkriege2 <jan@jkrieger.de>
2023-05-13 14:44:40 +0200

    add ReadTheDocs build

---
+++ doc/sphinx/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'JKQTPlotter'
+copyright = '2008-2023, Jan Krieger'
+author = 'Jan Krieger'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+html_extra_path = ['../build/html']
+
+import subprocess
+subprocess.call('cd ../.. ; doxygen', shell=True)
