be48ed84ba9d7c94844e232101f9b6446a94a61a
Social_Mean <Social_mean@126.com>
2024-03-18 17:42:32 +0800

    Add-qt5.12.12-compatibility (#127)
    add qt5.12.12 compatibility
    
    QList::swapItemsAt was introduced in Qt 5.13.
    and more ,,, thanks to https://github.com/Social-Mean

--- examples/libexampletools/jkqtpexampleapplication.h
+++ examples/libexampletools/jkqtpexampleapplication.h
@@ -44,7 +44,7 @@ protected:
         inline Data(const std::function<void(void)>& f_):
             type(FunctorType), f(f_), plotf(), p(nullptr)
         {}
-        inline Data(JKQTPlotter* p_):
+        inline Data(JKQTPlotter* p_=nullptr):
             type(PlotterType), f(), plotf(), p(p_)
         {}
         inline Data(std::function<JKQTPlotter*(void)> p_):
